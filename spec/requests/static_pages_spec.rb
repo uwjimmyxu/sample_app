5bd96c3774bc3842c909f7284b2afdc9d747c497
Jimmy <uwjimmyxu@gmail.com>
2012-12-01 18:57:21 -0800

    Added Contact Page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -37,6 +37,14 @@ describe "StaticPages" do
       end
    end
   
-   describe "Dynamic Title" do
+   describe "Contact page" do
+      it "should have the content 'Contact Us'" do
+         visit '/static_pages/contact'
+         page.should have_content('Contact Us')
+      end
+      it "should have the right title" do
+         visit '/static_pages/contact'
+         page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact")
+      end
    end
 end
