a4f41199ab51a88a6cc32844d06ba0ea1d3f3aa0
Jimmy <uwjimmyxu@gmail.com>
2012-12-01 19:53:02 -0800

    Finished chapter 3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+   let(:base_title) {"Ruby on Rails Tutorial Sample App |"}
+
    describe "Home page" do
       it "should have the content 'Sample App'" do
          visit '/static_pages/home'
@@ -9,7 +11,7 @@ describe "StaticPages" do
 
       it "should have the right title" do
          visit '/static_pages/home'
-         page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+         page.should have_selector('title', :text => "#{base_title} Home")
       end
    end
 
@@ -21,7 +23,7 @@ describe "StaticPages" do
 
       it "should have the right title" do
          visit '/static_pages/help'
-         page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+         page.should have_selector('title', :text => "#{base_title} Help")
       end
    end
 
@@ -33,7 +35,7 @@ describe "StaticPages" do
 
       it "should have the right title" do
          visit '/static_pages/about'
-         page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About")
+         page.should have_selector('title', :text => "#{base_title} About")
       end
    end
   
