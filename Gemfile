484d08da279f880004cd8d3ce9252f785af99b8b
Jimmy <uwjimmyxu@gmail.com>
2012-12-02 20:52:44 -0800

    Add a full_title helper

--- Gemfile
+++ Gemfile
@@ -8,6 +8,9 @@ gem 'rails', '3.2.9'
 group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.11.0'
+  gem 'guard-rspec', '1.2.1'
+  gem 'guard-spork', '1.2.0'
+  gem 'spork', '0.9.2'
 end
 
 # Gems used only for assets and not required
@@ -22,6 +25,8 @@ gem 'jquery-rails'
 
 group :test do
   gem 'capybara', '1.1.2'
+  gem 'rb-fsevent', '0.9.1', :require => false
+  gem 'growl', '1.0.3'
 end 
 
 group :production do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,16 +9,21 @@ describe "StaticPages" do
          page.should have_content('Sample App')
       end
 
-      it "should have the right title" do
+      it "should have base title" do
          visit '/static_pages/home'
-         page.should have_selector('title', :text => "#{base_title} Home")
+         page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
+      end
+
+      it "should nto have a custom page title" do
+         visit '/static_pages/home'
+         page.should_not have_selector('title', :text => '| Home')
       end
    end
 
    describe "Help page" do
       it "should have the content 'Help'" do
          visit '/static_pages/help'
-         page.should have_content('Help')
+         page.should have_content('Helpsfsdf')
       end
 
       it "should have the right title" do
